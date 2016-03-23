b27c54363b76dce4307aeec5b515c3a7fed4b20d
Andrew Keller <andrew.keller.09@gmail.com>
2016-03-22 23:24:15 -0700

    WIP

--- src/Resonators.jl
+++ src/Resonators.jl
@@ -5,11 +5,11 @@ using PainterQB.VNA
 
 export search_sidecoupled
 
-function search_sidecoupled(ins::InstrumentVNA, startf::Real, stopf::Real, cutoff=0.3, resolve_q=5e5, markers=1:9)
+function search_sidecoupled(ins::InstrumentVNA, startf::Real, stopf::Real, cutoff=0.3, span=5e5, markers=1:9)
     startf > stopf && error("Start frequency > stop frequency.")
 
     # Calculate frequency steps
-    stepf = stopf/resolve_q
+    stepf = stopf/span
     numpts = length(startf:stepf:stopf)
 
     configure(ins, FrequencyStart, startf)
@@ -17,7 +17,7 @@ function search_sidecoupled(ins::InstrumentVNA, startf::Real, stopf::Real, cutof
     configure(ins, NumPoints, 4001)
     configure(ins, IFBandwidth, 50)
 
-    configure(ins, Graphs, [1,2])
+    configure(ins, Graphs, [1])
     configure(ins, NumTraces, 1)
     configure(ins, VNA.LogMagnitude, 1, 1)
 
@@ -25,11 +25,39 @@ function search_sidecoupled(ins::InstrumentVNA, startf::Real, stopf::Real, cutof
     mx = search(ins,
         [MarkerSearch(:RightPeak, 1, 1, m, cutoff, VNA.Negative()) for m in markers]...)
 
-    for m in 2:length(mx)
+    isnan(mx[1]) && configure(ins, Marker, 1, false)
+    for m in 2:length(markers)
         (mx[m] == mx[m-1] || isnan(mx[m])) && configure(ins, Marker, m, false)
     end
 
-    mx
+    count = 0
+    fs = Float64[]
+    for m in markers
+        if inspect(ins, Marker, m) == true
+            count += 1
+            push!(fs, mx[m])
+        end
+    end
+
+    if count == 0
+        warning("No peaks were found.")
+        return
+    end
+
+    configure(ins, NumTraces, count)
+    for i in 1:count
+        configure(ins, VNA.PolarComplex, 1, i)
+        configure(ins, FrequencyCenter, fs[i], 1, i)
+        configure(ins, FrequencySpan, span, 1, i)
+    end
+
+    count == 2 && configure(ins, Graphs, [1 2])
+    3 <= count <= 4 && configure(ins, Graphs, [1 2; 3 4])
+    5 <= count <= 6 && configure(ins, Graphs, [1 2 3; 4 5 6])
+    7 <= count <= 8 && configure(ins, Graphs, [1 2 3 4; 5 6 7 8])
+    count == 9 && configure(ins, Graphs, [1 2 3; 4 5 6; 7 8 9])
+    10 <= count <= 12 && configure(ins, Graphs, [1 2 3 4; 5 6 7 8; 9 10 11 12])
+    13 <= count && configure(ins, Graphs, [1 2 3 4; 5 6 7 8; 9 10 11 12])
 
 end
 
